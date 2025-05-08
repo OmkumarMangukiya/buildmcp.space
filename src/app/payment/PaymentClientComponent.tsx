fe539a318ebb586c205a8233ac8c5a382f7dc355
OmkumarMangukiya <ommangukiya8+github@gmail.com>
2025-05-08 11:16:37 +0530

    build error fixed

--- src/app/payment/PaymentClientComponent.tsx
+++ src/app/payment/PaymentClientComponent.tsx
@@ -54,7 +54,7 @@ const validatePromoCode = (code: string, planInterval: string) => {
       code: 'ERBD21'
     };
   }
-  return { valid: false, discountPercent: 0 };
+  return { valid: false, discountPercent: 0, code: null };
 };
 
 export default function PaymentClientComponent() {
